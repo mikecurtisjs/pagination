906cdd73428d30e87a460601d198c5bf651d5772
Michael Curtis <mikecurtisjs@gmail.com>
2019-09-12 10:23:06 -0700

    Build

---
+++ build/precache-manifest.b8e42c320565868cb404418ebc322abf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770dfc548e6e87c5b13b592c8a8dec9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c82072dc8891cc08af9",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "494e8304205dea2fc39b",
+    "url": "/static/js/2.3de41033.chunk.js"
+  },
+  {
+    "revision": "9c82072dc8891cc08af9",
+    "url": "/static/js/main.88a360ea.chunk.js"
+  },
+  {
+    "revision": "7082de16caa60e78bace",
+    "url": "/static/js/runtime~main.80d3ab00.js"
+  }
+]);
